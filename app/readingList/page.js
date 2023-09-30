f41a129b93c83f2763b921010145ec80eeb3f3d4
ALBARAA REDA <albaraareda18@gmail.com>
2023-09-30 23:01:10 +0300

    adding key in reading list

--- app/readingList/page.js
+++ app/readingList/page.js
@@ -10,7 +10,7 @@ const ReadingList = ({ items }) => {
       <h2 className=" appTitle text-center py-4">My Reading List</h2>
       {listBooks.length > 0 &&
         listBooks.map((book, i) => (
-          <div className="max-h-screen flex flex-col gap-y-5 py-5">
+          <div key={i} className="max-h-screen flex flex-col gap-y-5 py-5">
             <Book key={i} items={book} />
           </div>
         ))}
